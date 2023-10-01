24a9aa0e9abf4af7173c85488162c3235a123367
unknown <nagasudhirpulla@gmail.com>
2023-10-01 22:00:59 +0530

    config changes for release

--- LearningSoftwareApp/LearningSoftwareApp.Android/MainActivity.cs
+++ LearningSoftwareApp/LearningSoftwareApp.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace LearningSoftwareApp.Droid
 {
-    [Activity(Label = "LearningSoftwareApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "LearningSoftware", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
